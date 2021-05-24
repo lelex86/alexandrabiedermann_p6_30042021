a6e87ae0f8f9d949688f4d00c529de6171b65e59
Alexandra Biedermann <nina@net2000.ch>
2021-05-24 11:36:46 +0200

    Modification du header Access-Control-Allow-Origin

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ mongoose
   .catch(() => console.log("Connexion à MongoDB échouée !"));
 app.use(helmet());
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
